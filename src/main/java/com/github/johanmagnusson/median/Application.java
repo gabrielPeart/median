385683a72fc48b9ae00d4671a64d7e13072d57e1
Johan Magnusson <johan.b.magnusson@gmail.com>
2017-05-25 12:05:42 +0200

    Stub out Stats class and add test cases from samples
    Introduces basic package structure and removes the code
    generated by the gradle init plugin.

---
+++ src/main/java/com/github/johanmagnusson/median/Application.java
@@ -0,0 +1,8 @@
+package com.github.johanmagnusson.median;
+
+public class Application {
+
+	public static void main(final String[] args) {
+		System.out.println("Hello, world!");
+	}
+}
---
+++ src/main/java/com/github/johanmagnusson/median/Stats.java
@@ -0,0 +1,8 @@
+package com.github.johanmagnusson.median;
+
+public class Stats {
+
+	public int calculateMedian(final int[] input) {
+		throw new UnsupportedOperationException("Not yet implemented");
+	}
+}
